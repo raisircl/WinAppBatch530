e809c5fbe925bdf95c4fc2b2608aa63107907b5d
raisircl <rai.verma@live.com>
2023-09-25 18:14:21 +0530

    timer and combo

--- Form4.cs
+++ Form4.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,42 @@ namespace WinAppBatch530
         public Form4()
         {
             InitializeComponent();
+        }
+        int i;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            foreach(var item in checkedListBox1.CheckedItems)
+            {
+                label1.Text += item.ToString() +"\n"; 
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text=comboBox1.SelectedItem.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum=listBox1.Items.Count;
+            i = 0;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(i<progressBar1.Maximum) {
+                listBox2.Items.Add(listBox1.Items[i]);
+                i++;
+                progressBar1.Value = i;
+            }
+            else
+            {
+                timer1.Stop();
+            }
         }
     }
 }
