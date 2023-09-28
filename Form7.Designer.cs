cfcf774de0fbd1eddcde34006ca335fed3c1bc31
raisircl <rai.verma@live.com>
2023-09-28 18:01:14 +0530

    dataset and datagrid

---
+++ Form7.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WinAppBatch530
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDNo = new System.Windows.Forms.TextBox();
+            this.txtDName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLoc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DNO";
+            // 
+            // txtDNo
+            // 
+            this.txtDNo.Location = new System.Drawing.Point(192, 55);
+            this.txtDNo.Name = "txtDNo";
+            this.txtDNo.Size = new System.Drawing.Size(309, 41);
+            this.txtDNo.TabIndex = 1;
+            // 
+            // txtDName
+            // 
+            this.txtDName.Location = new System.Drawing.Point(192, 120);
+            this.txtDName.Name = "txtDName";
+            this.txtDName.Size = new System.Drawing.Size(309, 41);
+            this.txtDName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DNAME";
+            // 
+            // txtLoc
+            // 
+            this.txtLoc.Location = new System.Drawing.Point(192, 183);
+            this.txtLoc.Name = "txtLoc";
+            this.txtLoc.Size = new System.Drawing.Size(309, 41);
+            this.txtLoc.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 36);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "LOC";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(193, 251);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(111, 50);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(310, 251);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(162, 50);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(478, 251);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(118, 50);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Del";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgv1
+            // 
+            this.dgv1.AllowUserToAddRows = false;
+            this.dgv1.AllowUserToDeleteRows = false;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(12, 336);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.ReadOnly = true;
+            this.dgv1.RowHeadersWidth = 51;
+            this.dgv1.RowTemplate.Height = 24;
+            this.dgv1.Size = new System.Drawing.Size(783, 194);
+            this.dgv1.TabIndex = 9;
+            this.dgv1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv1_CellClick);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(602, 251);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(166, 50);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 36F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 566);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtLoc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDNo);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.8F);
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form7";
+            this.Text = "Form7";
+            this.Load += new System.EventHandler(this.Form7_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDNo;
+        private System.Windows.Forms.TextBox txtDName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLoc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
---
+++ Form7.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppBatch530
+{
+    public partial class Form7 : Form
+    {
+        public Form7()
+        {
+            InitializeComponent();
+        }
+        DataSet ds = new DataSet();
+        
+        DataTable dt=new DataTable("tbldept");
+        
+        DataColumn dc1 = new DataColumn("DNo");
+        DataColumn dc2 = new DataColumn("DName");
+        DataColumn dc3 = new DataColumn("Loc");
+
+        int ci = -1;
+        private void Form7_Load(object sender, EventArgs e)
+        {
+            dt.Columns.Add(dc1);    
+            dt.Columns.Add(dc2);
+            dt.Columns.Add(dc3);
+            
+            ds.Tables.Add(dt);  
+            
+            dgv1.DataSource = ds.Tables["tbldept"];
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            DataRow row = ds.Tables["tbldept"].NewRow();
+            row["Dno"]=txtDNo.Text;
+            row["DName"]=txtDName.Text;
+            row["Loc"] = txtLoc.Text;
+            ds.Tables["tbldept"].Rows.Add(row);
+            reset();
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+        void reset()
+        {
+            ci = -1;
+            txtDNo.Text = "";
+            txtDName.Text = "";
+            txtLoc.Text = "";
+            txtDNo.Focus();
+        }
+
+        private void dgv1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex>=0)
+            {
+                ci= e.RowIndex;
+                txtDNo.Text = dgv1.Rows[ci].Cells["DNo"].Value.ToString();
+                txtDName.Text = dgv1.Rows[ci].Cells["DName"].Value.ToString();
+                txtLoc.Text = dgv1.Rows[ci].Cells["Loc"].Value.ToString();
+
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if(ci>=0)
+            {
+                DataRow dr = ds.Tables["tblDept"].Rows[ci];
+                dr["Dno"] = txtDNo.Text;
+                dr["DName"] = txtDName.Text;
+                dr["Loc"] = txtLoc.Text;
+                reset();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (ci >= 0)
+            {
+                ds.Tables["tblDept"].Rows.RemoveAt(ci); 
+                reset();
+            }
+        }
+    }
+}
